7de6bc59aa8285fdaf6674bbd3d4d2f1c808cee4
Paweł <tatarynowiczpawel2@gmail.com>
2019-06-19 20:43:26 +0200

    Show new article button to signed in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -3,9 +3,41 @@ require 'rails_helper'
 RSpec.feature "Listing Articles" do
 
   before do
-    john = User.create(email: "john@example.com", password: "password")
-    @article1 = Article.create(title: "The first article", body: "Lorem Ipsum dolor sit amet", user: john)
-    @article2 = Article.create(title: "The second article", body: "Lorem Ipsum 2", user: john)
+    @john = User.create(email: "john@example.com", password: "password")
+    @article1 = Article.create(title: "The first article", body: "Lorem Ipsum dolor sit amet", user: @john)
+    @article2 = Article.create(title: "The second article", body: "Lorem Ipsum 2", user: @john)
+  end
+
+  scenario "With articles created and user not signed in" do
+
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
+
+  end
+
+  scenario "With articles created and user signed in" do
+    login_as @john
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
+
   end
 
   scenario "A user lists all articles" do
@@ -21,6 +53,7 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
 
+
   end
 
   scenario "A user has no articles" do
@@ -28,13 +61,13 @@ RSpec.feature "Listing Articles" do
     visit "/"
 
     expect(page).not_to have_content(@article1.title)
-    expect(page).not_to  have_content(@article1.body)
+    expect(page).not_to have_content(@article1.body)
 
-    expect(page).not_to  have_content(@article2.title)
-    expect(page).not_to  have_content(@article2.body)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
 
-    expect(page).not_to  have_link(@article1.title)
-    expect(page).not_to  have_link(@article2.title)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
 
     within("h1#no-articles") do
       expect(page).to have_content("No Articles Created")
